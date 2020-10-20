3b91b02372c8c30afafbedb26376f3939c3b72f2
C-Lodder <lodder15@hotmail.co.uk>
2020-10-20 18:45:16 +0100

    Add build scripts

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# github-dark-theme
+# GDT (GitHub Dark Theme)
+
+Unlike other solutions out there that inject a 1.3MB CSS file, GDT primarily utilises GitHub's CSS variables, resulting in a clean, 13kb minfied CSS file that won't effect the performance if your GitHub experience.
+
+## Build tasks
+- Minify CSS:
+```bash
+node build.js
+```
+
+## Contributing
+- Clone the repository:
+```bash
+git clone https://github.com/C-Lodder/github-dark-theme.git <path>
+```
+- Go to the folder:
+```bash
+cd <path>
+```
+- Install dependencies:
+```bash
+npm ci
+```
+- Checkout a new branch:
+```bash
+git checkout -b <branch name>
+```
---
+++ build.js
@@ -0,0 +1,20 @@
+const { Worker, isMainThread, parentPort } = require('worker_threads')
+const { readFile, writeFile } = require('fs').promises
+const CleanCSS = require('clean-css')
+
+const ProcessCss = async () => {
+  const file = await readFile(`${__dirname}/css/github-dark.css`, 'utf8')
+  const css = await new CleanCSS({}).minify(file)
+
+  writeFile(`${__dirname}/css/github-dark.min.css`, css.styles, { flag: 'wx' }, () => true)
+}
+
+if (isMainThread) {
+  // Save the main thread incase we need it later
+  const worker = new Worker(__filename)
+  worker.postMessage('message')
+} else {
+  parentPort.once('message', () => {
+    ProcessCss()
+  })
+}
